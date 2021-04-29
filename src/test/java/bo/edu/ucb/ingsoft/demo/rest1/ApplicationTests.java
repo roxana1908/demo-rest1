9a1cf8211c2c13703276d7753d3f82587cafbcb6
Roxana <roxana.vargas.telleria@gmail.com>
2021-04-28 23:30:08 -0400

    Subiendo primera version del proyecto

---
+++ src/test/java/bo/edu/ucb/ingsoft/demo/rest1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.demo.rest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
